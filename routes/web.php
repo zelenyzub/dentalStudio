a3f21bee37cd4813b6be46b6f4a8522fa95af7b3
zelenyzub <136793603+zelenyzub@users.noreply.github.com>
2024-10-16 18:10:35 +0200

    laravel UI added

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/kartoni', [App\Http\Controllers\RecordController::class, 'record'])->name('kartoni');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
